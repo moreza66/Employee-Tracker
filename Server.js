21576520c895a7f85d0d8d6301ae2c00dff43324
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2022-11-02 17:29:15 -0500

    Initial Prompt

--- Server.js
+++ Server.js
@@ -16,3 +16,55 @@ const connection = mysql.createConnection({
     console.log('WELCOME TO EMPLOYEE TRACKER ' + connection.threadId);
     afterConnection();
   });
+
+  function options() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'WELCOME TO EMPLOYEE TRACKER! Lets Have Some Action!?',
+            choices: [
+                    'View all employees',
+                    'View all departments',
+                    'View all roles',
+                    'Add an employee',
+                    'Add a department',
+                    'Add a role',
+                    'Update employee role',
+                    'Delete an employee',
+                    'EXIT'
+                    ]
+            }).then(function (answer) {
+                switch (answer.action) {
+                    case 'View all employees':
+                        viewEmployees();
+                        break;
+                    case 'View all departments':
+                        viewDepartments();
+                        break;
+                    case 'View all roles':
+                        viewRoles();
+                        break;
+                    case 'Add an employee':
+                        addEmployee();
+                        break;
+                    case 'Add a department':
+                        addDepartment();
+                        break;
+                    case 'Add a role':
+                        addRole();
+                        break;
+                    case 'Update employee role':
+                        updateRole();
+                        break;
+                    case 'Delete an employee':
+                        deleteEmployee();
+                        break;
+                    case 'EXIT': 
+                        exitApp();
+                        break;
+                    default:
+                        break;
+                }
+        })
+};
